53afc74e85c5401c272a58e630a8fb75b2df4253
id774 <idnanashi@gmail.com>
2014-01-26 23:00:58 +0900

    21 Version bump to the next release

--- lib/naivebayes/version.rb
+++ lib/naivebayes/version.rb
@@ -1,10 +1,10 @@
 # Name::      NaiveBayes::Version
 # Author::    774 <http://id774.net>
 # Created::   Nov 24, 2013
-# Updated::   Jan 22, 2014
+# Updated::   Jan 26, 2014
 # Copyright:: 774 Copyright (c) 2013-2014
 # License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
 
 module NaiveBayes
-  VERSION = "0.1.0"
+  VERSION = "0.1.0-devel"
 end
--- spec/lib/naivebayes_spec.rb
+++ spec/lib/naivebayes_spec.rb
@@ -6,6 +6,6 @@ describe NaiveBayes do
   context "VERSION" do
     subject { NaiveBayes::VERSION }
 
-    it { expect(subject).to eq "0.1.0" }
+    it { expect(subject).to eq "0.1.0-devel" }
   end
 end
